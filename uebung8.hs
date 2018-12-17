123a7f72b8b171af71fb52ec92e92bfcc4c3b8b9
Marvin Plum <lnxtt1@gmail.com>
2018-12-17 11:46:12 +0100

    multList implementiert und Fehlerbehebungen

--- uebung8.hs
+++ uebung8.hs
@@ -3,12 +3,12 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-pow :: (Int, Int) -> Int
+pow :: Int -> Int -> Int
 pow _ 0 = 1
-pow _ 1 = a
+pow a 1 = a
 pow a b = a * pow a (b-1)
 
-isDiv :: (Int, Int) -> Bool
+isDiv :: Int -> Int -> Bool
 isDiv 0 _ = True
 isDiv _ 0 = False
 isDiv a b | a < b = False
@@ -17,3 +17,8 @@ isDiv a b = isDiv (a-b) b
 sumUp :: [Int] -> Int
 sumUp [] = 0
 sumUp (x:xs) = x + sumUp xs
+
+multList :: [Int] -> [Int] -> [Int]
+multList _ [] = []
+multList [] _ = []
+multList (x:xs) (y:xy) = ((x * y): multList xs xy)
